0858c600ba56e8a24f9f980e1ab735c49c923cbc
김영수 <presentis252@hanyang.ac.kr>
2020-02-18 12:03:29 +0900

    js practice#2

--- bootstrap/example6/js/example.js
+++ bootstrap/example6/js/example.js
@@ -1,3 +1,6 @@
-function change() {
-  document.body.className = 'changed';
+function setRed() {
+  var temp = document.getElementsByClassName('text');
+  for(var i=0;i<temp.length;i++){
+    temp[i].style.color=red;
+  }
 }
