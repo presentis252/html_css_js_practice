e1f04203c250963f075817c360cce247cce61209
김영수 <presentis252@hanyang.ac.kr>
2020-03-02 22:46:21 +0900

    event처리 연습

---
+++ bootstrap/example7/js/example.js
@@ -0,0 +1,10 @@
+function buttonClickEvent(event) {
+  switch(event.target.id){
+    case 'button1':
+      alert('button 1');
+      break;
+    case 'button2':
+      alert('button 2');
+      break;
+  }
+}
